d946ddc188483e8be907c842233bb69c80cca4f5
George Magklaras <georgios@mail.steelcyber.com>
2021-06-15 17:54:07 +0200

    Update itpslschema.sql
    entered description and License info

--- server/itpslschema.sql
+++ server/itpslschema.sql
@@ -1,3 +1,24 @@
+# itpslschema.sql - SQL template to make the server database relational tables for each of the POFR clients  
+#POFR - Penguin OS Forensic (or Flight) Recorder - 
+#A program that collects stores and organizes for further analysis process execution, file access and network/socket endpoint data from the Linux Operating System 
+#and derivatives.
+#Copyright (C) 2021 Georgios Magklaras
+
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 -- MySQL dump 10.13  Distrib 5.1.44, for redhat-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: itpsl
