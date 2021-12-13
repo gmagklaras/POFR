e0a08573b05499a2ed446d1eef19862a5daa0b8c
Dr. George Magklaras <georgios@mail.steelcyber.com>
2021-12-13 16:36:53 +0100

    New set of client scripts to test syncing of proc and net data

---
+++ client/newstopclient.pl
@@ -0,0 +1,52 @@
+#!../pofrperl/bin/perl -w -I ../pofrperl/lib/5.34.0/x86_64-linux -I ../pofrperl/lib/5.34.0
+#
+use lib '../pofrperl/lib/site_perl/5.34.0';
+
+#stopclient.pl: A script to stop the POFR client processes
+
+#POFR - Penguin OS Forensic (or Flight) Recorder - 
+#A program that collects stores and organizes for further analysis process execution, file access and network/socket endpoint data from the Linux Operating System 
+#and derivatives.
+#Copyright (C) 2021 Georgios Magklaras
+
+#Meteorologisk Institutt/The Norwegian Meteorological Institute, hereby disclaims all copyright interest in the program
+#`POFR' (A program that collects stores and organizes for further analysis process execution, file access and network/socket endpoint data from the Linux Operating System and derivatives ) written by Georgios Magklaras, 
+#Arnstein Orten, February 1 2021, Assistant Director for IT infrastructure
+
+
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use IO::File;
+use Time::HiRes qw(usleep clock_gettime gettimeofday clock_getres CLOCK_REALTIME ITIMER_REAL ITIMER_VIRTUAL ITIMER_PROF ITIMER_REALPROF);
+use POSIX;
+
+#my $startclientpid="$$";
+my $sendprocpid;
+my $scanprocpid;
+
+#Sanity checks
+#Are we root?
+#
+chomp (my $result=`ps auxwww | grep sendproc.pl | grep -v grep`);
+chomp (my $procres=`ps auxwww | grep newscanproc.pl | grep -v grep`);
+
+if ($result || $procres) {
+	system "killall -9 newscanproc.pl; sleep 3; killall -9 sendproc.pl; rm .scanpid; rm .sendpid";
+} else {
+	print "stopclient.pl Info: No active POFR client processes to stop. Exiting. \n";
+}
+
+
+		
