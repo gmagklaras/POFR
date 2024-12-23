051f16f640d7acb208f7c5490e596ad8cf340479
Dr. George Magklaras <georgios@mail.steelcyber.com>
2024-12-23 20:47:35 +0100

    Turning the server code to use POFR Perl 5.40.0

--- server/pofrsreg.pl
+++ server/pofrsreg.pl
@@ -1,6 +1,6 @@
-#!../pofrperl/bin/perl -w -I ../pofrperl/lib/5.38.2/x86_64-linux -I ../pofrperl/lib/5.38.2 -I ../lib
+#!../pofrperl/bin/perl -w -I ../pofrperl/lib/5.40.0/x86_64-linux -I ../pofrperl/lib/5.40.0 -I ../lib
 #
-use lib '../pofrperl/lib/site_perl/5.38.2';
+use lib '../pofrperl/lib/site_perl/5.40.0';
 
 #pofrsreg.pl : SERVER side module that handles the POFR client registration requests
 #POFR - Penguin OS Forensic (or Flight) Recorder - 
